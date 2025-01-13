7f95b0e3e34fe8f3c8e8ea8c356edbe25a389287
rrrRex-pages <19588423@qq.com>
2025-01-13 11:31:54 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,16 @@
 #include<iostream>
 #include<windows.h>
 #include<thread>
+#include<chrono>
 using namespace std;
 
 #define KEY_DOWN GetAsyncKeyState
 #define ME MOUSEEVENTF
-
+int delay;
 char trig;
 bool flag;
 bool SwitchTrue;
+int cps;
 
 void Switch()
 {
@@ -29,35 +31,38 @@ void Right()
 {
 	while (!KEY_DOWN(VK_LSHIFT))
 	{
-		while (flag||KEY_DOWN(VK_RBUTTON))
+		while (flag || KEY_DOWN(VK_RBUTTON))
 		{
 			while (KEY_DOWN(trig))
 			{
 				mouse_event(MOUSEEVENTF_RIGHTDOWN | MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0);
+				this_thread::sleep_for(chrono::milliseconds(delay));
 			}
 		}
-		
+
 	}
 }
 void Left()
 {
 	while (!KEY_DOWN(VK_LSHIFT))
 	{
-		while (flag||KEY_DOWN(VK_LBUTTON))
+		while (flag || KEY_DOWN(VK_LBUTTON))
 		{
 			while (KEY_DOWN(trig))
 			{
-				mouse_event(MOUSEEVENTF_LEFTDOWN|MOUSEEVENTF_LEFTUP,0,0,0,0);
+				mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+				this_thread::sleep_for(chrono::milliseconds(delay));
 			}
 		}
-		
+
 	}
 }
 int main()
 {
 	bool lr;
-	cout << "Input 3 values:\n1.left/right(0 for right 1 for left\n2.trig:only alphabets!!!! please input those alphabets CAPITALIZED!!!!\n3.Switch: 0 for trig+mouse button, 1 for trig+mouse button or trig only. switch by rshift\nto exit, press lshift\n";
-	cin >> lr >> trig >> SwitchTrue;
+	cout << "Input 4 values:\n1.left/right(0 for right 1 for left\n2.trig:only alphabets!!!! please input those alphabets CAPITALIZED!!!!\n3.Switch: 0 for trig+mouse button, 1 for trig+mouse button or trig only. switch by rshift\n3. input your expected cps(click per second)\nto exit, press lshift\n";
+	cin >> lr >> trig >> SwitchTrue >> cps;
+	delay = (1000 / cps) - 15;
 	if (SwitchTrue)
 	{
 		thread t(Switch);
